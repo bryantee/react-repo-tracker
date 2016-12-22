682c64d444adb716391a6d88e6212ed99381343b
bryantee <bryantee@gmail.com>
2016-12-21 17:07:40 -0700

    Add store, wire up and test

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // require('babel-polyfill');
 import * as actions from './actions/index'
 import { initialRepositoryState, repositoryReducer } from './reducers/index'
+import store from './store'
 
-console.log(repositoryReducer([ { name: 'TEST REPO', rating: null }], actions.rateRepo('TEST REPO', 5)));
+// console.log(repositoryReducer([ { name: 'TEST REPO', rating: null }], actions.rateRepo('TEST REPO', 5)));
+store.dispatch(actions.addRepo('joe'));
+console.log(store.getState());
